5dccd6afd3e6031e54de873ac155a4258009fd36
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-01-13 21:06:25 -0600

    Dialog added

---
+++ src/addTask.js
@@ -0,0 +1,22 @@
+// addTask.js
+export const initialize = () => {
+  const pressTaskBtn = () => {
+    const addTaskBtn = document.getElementById("addTaskBtn");
+    const taskModal = document.getElementById("taskModal");
+    addTaskBtn.addEventListener("click", () => {
+      taskModal.showModal();
+      taskModal.style.display = "flex";
+    });
+  };
+
+  const pressDialogBtn = () => {
+    const closeDialogBtn = document.getElementById("closeBtn");
+    const taskModal = document.getElementById("taskModal");
+    closeDialogBtn.addEventListener("click", () => {
+      taskModal.close();
+      taskModal.style.display = "none";
+    });
+  };
+
+  return { pressTaskBtn, pressDialogBtn };
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css";
+import { initialize } from "./addTask.js";
+import { greeting } from "./addTask.js";
+document.addEventListener("DOMContentLoaded", () => {
+  const { pressTaskBtn, pressDialogBtn } = initialize();
+  pressTaskBtn();
+  pressDialogBtn();
+});
